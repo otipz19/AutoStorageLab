d0564eb9bfc4ac2aa215123e47b10395d0066acc
otipz19 <tasitwork@gmail.com>
2024-03-29 12:00:52 +0200

    Completed ProductTable

--- src/main/model/data/tables/ProductTable.java
+++ src/main/model/data/tables/ProductTable.java
@@ -1,6 +1,7 @@
 package main.model.data.tables;
 
 import main.model.data.DataContext;
+import main.model.data.records.GroupRecord;
 import main.model.data.records.ProductRecord;
 import main.model.dto.Mapper;
 import main.model.dto.ProductDto;
@@ -10,6 +11,7 @@ import main.model.exceptions.crud.ProductNotFoundException;
 import main.model.valueObjects.GroupName;
 import main.model.valueObjects.ProductName;
 
+import javax.xml.crypto.Data;
 import java.util.*;
 
 public class ProductTable implements IProductTable {
@@ -43,26 +45,11 @@ public class ProductTable implements IProductTable {
         //here is group existence already validated
         UUID groupId = getGroupIdByGroupName(toCreate.getGroupName());
         ProductRecord record = Mapper.map(toCreate, id, groupId);
-        addRecordToIndexes(id, record);
+        addRecordToIndexes(record);
     }
 
     private static UUID getGroupIdByGroupName(GroupName groupName) {
         return DataContext.getInstance().getGroupTable().get(groupName).getId();
-    }
-
-    private void addRecordToIndexes(UUID id, ProductRecord record) {
-        primaryKey.put(id, record);
-        nameIndex.put(record.getName(), record);
-        addRecordToGroupIdIndex(record);
-    }
-
-    private void addRecordToGroupIdIndex(ProductRecord record) {
-        var groupIndex = groupIdIndex.get(record.getGroupId());
-        if(groupIndex.isEmpty()){
-            groupIndex = new ArrayList<ProductRecord>();
-        }
-        groupIndex.add(record);
-        groupIdIndex.put(record.getGroupId(), groupIndex);
     }
 
     @Override
@@ -83,11 +70,65 @@ public class ProductTable implements IProductTable {
     public void deleteByGroupId(UUID groupId) {
         throwIfGroupDoesNotExist(groupId);
         List<ProductRecord> records = groupIdIndex.get(groupId);
-        for(ProductRecord record : records){
+        for (ProductRecord record : records) {
             removeRecordFromPrimaryKey(record);
             removeRecordFromNameIndex(record);
         }
         groupIdIndex.remove(groupId);
+    }
+
+    @Override
+    public void update(UUID id, ProductDto toUpdate) {
+        //existence already validated in get
+        ProductRecord oldRecord = get(id);
+        updateRecord(toUpdate, oldRecord);
+    }
+
+    @Override
+    public void update(ProductName name, ProductDto toUpdate) {
+        ProductRecord oldRecord = get(name);
+        updateRecord(toUpdate, oldRecord);
+    }
+
+    private void updateRecord(ProductDto toUpdate, ProductRecord oldRecord) {
+        GroupRecord oldGroup = DataContext.getInstance().getGroupTable().get(oldRecord.getGroupId());
+        UUID groupId = oldGroup.getId();
+        if(!toUpdate.getGroupName().equals(oldGroup.getName())){
+            groupId = DataContext.getInstance().getGroupTable().get(toUpdate.getGroupName()).getId();
+        }
+        ProductRecord updatedRecord = Mapper.map(toUpdate, oldRecord.getId(), groupId);
+        addRecordToPrimaryKey(updatedRecord);
+        if (!updatedRecord.getName().equals(oldRecord.getName())) {
+            removeRecordFromNameIndex(oldRecord);
+            addRecordToGroupIdIndex(updatedRecord);
+        }
+        if(!oldGroup.getId().equals(groupId)){
+            removeRecordFromGroupIdIndex(oldRecord);
+            addRecordToGroupIdIndex(updatedRecord);
+        }
+    }
+
+    private void addRecordToIndexes(ProductRecord record) {
+        addRecordToPrimaryKey(record);
+        addRecordToNameIndex(record);
+        addRecordToGroupIdIndex(record);
+    }
+
+    private void addRecordToPrimaryKey(ProductRecord record) {
+        primaryKey.put(record.getId(), record);
+    }
+
+    private void addRecordToNameIndex(ProductRecord record){
+        nameIndex.put(record.getName(), record);
+    }
+
+    private void addRecordToGroupIdIndex(ProductRecord record) {
+        var groupIndex = groupIdIndex.get(record.getGroupId());
+        if (groupIndex.isEmpty()) {
+            groupIndex = new ArrayList<ProductRecord>();
+        }
+        groupIndex.add(record);
+        groupIdIndex.put(record.getGroupId(), groupIndex);
     }
 
     private void removeRecordFromIndexes(ProductRecord record) {
@@ -107,39 +148,28 @@ public class ProductTable implements IProductTable {
     private void removeRecordFromGroupIdIndex(ProductRecord record) {
         var groupIndex = groupIdIndex.get(record.getGroupId());
         groupIndex.remove(record);
-        groupIdIndex.put(record.getGroupId(), groupIndex);
-    }
-
-    @Override
-    public void update(UUID id, ProductDto toUpdate) {
-
-    }
-
-    @Override
-    public void update(ProductName name, ProductDto toUpdate) {
-
     }
 
     private void throwIfDoesNotExist(UUID id) {
-        if(!primaryKey.containsKey(id)){
+        if (!primaryKey.containsKey(id)) {
             throw new ProductNotFoundException(id);
         }
     }
 
-    private void throwIfDoesNotExist(ProductName name){
-        if(!nameIndex.containsKey(name)){
+    private void throwIfDoesNotExist(ProductName name) {
+        if (!nameIndex.containsKey(name)) {
             throw new ProductNotFoundException(name);
         }
     }
 
-    private void throwIfGroupDoesNotExist(UUID groupId){
-        if(!groupIdIndex.containsKey(groupId)){
+    private void throwIfGroupDoesNotExist(UUID groupId) {
+        if (!groupIdIndex.containsKey(groupId)) {
             throw new GroupNotFoundException(groupId);
         }
     }
 
-    private void throwIfExists(ProductName name){
-        if(nameIndex.containsKey(name)){
+    private void throwIfExists(ProductName name) {
+        if (nameIndex.containsKey(name)) {
             throw new ProductNameAlreadyExists(name);
         }
     }
