dc632ae1e65373aee5ee8a2ff62999ddd068008f
otipz19 <tasitwork@gmail.com>
2024-04-09 18:25:27 +0300

    Changed GroupScreen layout

--- src/main/ui/screens/groupScreen/GroupScreen.java
+++ src/main/ui/screens/groupScreen/GroupScreen.java
@@ -24,45 +24,71 @@ public class GroupScreen extends JPanel {
 
     public GroupScreen() {
         App.getInstance().setTitle("Group Details");
-        setLayout(null);
-        setBackground(new Color(0xe9f2fb));
+        setLayout(new BorderLayout());
 
-        groupNameField = new GroupNameField();
-        groupNameField.setBounds(158, 58, 580, 50);
-        add(groupNameField);
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(50, 100, 50, 100));
+        mainPanel.setBackground(new Color(0xe9f2fb));
+        mainPanel.add(createActionsPanel(), BorderLayout.NORTH);
+        mainPanel.add(createSearchResultPanel(), BorderLayout.CENTER);
 
-        descriptionArea = new GroupDescriptionArea();
-        descriptionArea.setBounds(158, 108, 580, 50);
-        add(descriptionArea);
-
-        searchField = new GroupProductsSearchField();
-        searchField.setBounds(158, 158, 580, 50);
-        add(searchField);
-
-        editNameBtn = new EditGroupNameButton();
-        editNameBtn.setBounds(748, 58, 50, 50);
-        add(editNameBtn);
-
-        editDescriptionBtn = new EditGroupDescriptionButton();
-        editDescriptionBtn.setBounds(748, 108, 50, 50);
-        add(editDescriptionBtn);
-
-        createProductBtn = new CreateProductButton();
-        createProductBtn.setBounds(748, 158, 50, 50);
-        add(createProductBtn);
-
-        searchResultsPanel = new JPanel();
-        searchResultsPanel.setBounds(158, 208, 580, 500);
-        add(searchResultsPanel);
+        add(mainPanel, BorderLayout.CENTER);
     }
 
-    public void setGroup(GroupDto groupDto){
+    private JPanel createActionsPanel(){
+        JPanel actionsPanel = new JPanel(new GridLayout(3, 1));
+        actionsPanel.add(createGroupNamePanel());
+        actionsPanel.add(createDescriptionPanel());
+        actionsPanel.add(createSearchPanel());
+        return actionsPanel;
+    }
+
+    private JPanel createGroupNamePanel() {
+        JPanel groupNamePanel = new JPanel(new BorderLayout());
+        groupNameField = new GroupNameField();
+        groupNameField.setSize(580, 50);
+        groupNamePanel.add(groupNameField, BorderLayout.CENTER);
+        editNameBtn = new EditGroupNameButton();
+        editNameBtn.setSize(50, 50);
+        groupNamePanel.add(editNameBtn, BorderLayout.EAST);
+        return groupNamePanel;
+    }
+
+    private JPanel createDescriptionPanel() {
+        JPanel descriptionPanel = new JPanel(new BorderLayout());
+        descriptionArea = new GroupDescriptionArea();
+        descriptionArea.setSize(580, 50);
+        descriptionPanel.add(descriptionArea, BorderLayout.CENTER);
+        editDescriptionBtn = new EditGroupDescriptionButton();
+        editDescriptionBtn.setSize(50, 50);
+        descriptionPanel.add(editDescriptionBtn, BorderLayout.EAST);
+        return descriptionPanel;
+    }
+
+    private JPanel createSearchPanel() {
+        JPanel searchPanel = new JPanel(new BorderLayout());
+        searchField = new GroupProductsSearchField();
+        searchField.setSize(580, 50);
+        searchPanel.add(searchField, BorderLayout.CENTER);
+        createProductBtn = new CreateProductButton();
+        createProductBtn.setSize(50, 50);
+        searchPanel.add(createProductBtn, BorderLayout.EAST);
+        return searchPanel;
+    }
+
+    private JPanel createSearchResultPanel() {
+        searchResultsPanel = new JPanel();
+        searchResultsPanel.setSize(580, 500);
+        return searchResultsPanel;
+    }
+
+    public void setGroup(GroupDto groupDto) {
         this.group = groupDto;
         groupNameField.setText(groupDto.getName().getValue());
         descriptionArea.setText(groupDto.getDescription());
     }
 
-    public GroupDto getGroupToUpdate(){
+    public GroupDto getGroupToUpdate() {
         return new GroupDto(groupNameField.getText(), descriptionArea.getText());
     }
 }
